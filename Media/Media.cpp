5f3c5bad7a43b8d633bfe34f47632e597a79e59f
Matanel <User@USER-THINK>
2018-04-17 16:12:31 +0300

    CreateSourceReader formed.

--- Media/Media.cpp
+++ Media/Media.cpp
@@ -1,48 +1,60 @@
 #include "stdafx.h"
 #include "Media.h"
 
-
 Media::Media()
 {
+	m_pVIDSource = NULL;
+	m_pVIDAttributes = NULL;
+	m_ppVIDDevices = NULL;
+	m_pAUDSource = NULL;
+	m_pAUDAttributes = NULL;
+	m_ppAUDDevices = NULL;
+
+	m_pReader = NULL;
 }
 
 void Media::createMediaFile()
 {
-	EnumerateDevices(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_AUDCAP_GUID);
-	EnumerateDevices(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID);
+	EnumerateDevices(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_AUDCAP_GUID, m_pAUDSource, m_pAUDAttributes, m_ppAUDDevices);
+	EnumerateDevices(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID, m_pVIDSource, m_pVIDAttributes, m_ppVIDDevices);
+
+	std::cout << "EnumerateDevices for both audio & video is done." << std::endl;
+
+	CreateSourceReader(m_pAUDSource);
+	CreateSourceReader(m_pVIDSource);
+
+	std::cout << "MFCreateSourceReaderFromMediaSource for both audio & video is done." << std::endl;
 }
 
-void Media::EnumerateDevices(GUID deviceType)
+void Media::EnumerateDevices(
+	GUID deviceType,
+	IMFMediaSource* m_pSource,
+	IMFAttributes* m_pAttributes,
+	IMFActivate** m_ppDevices)
 {
-	IMFMediaSource *pSource = NULL;
-	IMFAttributes *pAttributes = NULL;
-	IMFActivate **ppDevices = NULL;
 	UINT32 count = 0;
 
 	CoInitialize(NULL);
 
-	HRESULT hr = MFCreateAttributes(&pAttributes, 1);
+	HRESULT hr = MFCreateAttributes(&m_pAttributes, 1);
 	if (FAILED(hr))
 	{
 		std::cout << "fail at MFCreateAttributes" << std::endl;
 	}
 
-	hr = pAttributes->SetGUID(
-		MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
-		deviceType
-	);
+	hr = m_pAttributes->SetGUID(MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE, deviceType);
 	if (FAILED(hr))
 	{
 		std::cout << "fail at SetGUID" << std::endl;
 	}
 
-	hr = MFEnumDeviceSources(pAttributes, &ppDevices, &count);
+	hr = MFEnumDeviceSources(m_pAttributes, &m_ppDevices, &count);
 	if (FAILED(hr))
 	{
 		std::cout << "fail at MFEnumDeviceSources	`" << std::endl;
 	}
 
-	hr = ppDevices[0]->ActivateObject(IID_PPV_ARGS(&pSource));
+	hr = m_ppDevices[0]->ActivateObject(IID_PPV_ARGS(&m_pSource));
 	if (FAILED(hr))
 	{
 		std::cout << "fail at ActivateObject" << std::endl;
@@ -55,7 +67,7 @@ void Media::EnumerateDevices(GUID deviceType)
 
 		// Try to get the display name.
 		UINT32 cchName;
-		hr = ppDevices[i]->GetAllocatedString(
+		hr = m_ppDevices[i]->GetAllocatedString(
 			MF_DEVSOURCE_ATTRIBUTE_FRIENDLY_NAME,
 			&szFriendlyName, &cchName);
 
@@ -68,6 +80,16 @@ void Media::EnumerateDevices(GUID deviceType)
 	}
 }
 
+void Media::CreateSourceReader(IMFMediaSource * m_pAUDSource)
+{
+	HRESULT hr = MFCreateSourceReaderFromMediaSource(m_pAUDSource, NULL, &m_pReader);
+
+	if (SUCCEEDED(hr))
+	{
+		//CreateMediaFileForReader(m_pReader); TODO - IMPLEMENT.
+		m_pReader->Release();
+	}
+}
 
 Media::~Media()
 {
--- Media/Media.h
+++ Media/Media.h
@@ -2,12 +2,27 @@
 #include <mfapi.h>
 #include <mfidl.h>
 #include <iostream>
+#include <Mfreadwrite.h>
+
 class Media
 {
 public:
 	Media();
 	void createMediaFile();//TODO change name
-	void EnumerateDevices(GUID deviceType);
+	void EnumerateDevices(
+		GUID deviceType,
+		IMFMediaSource* m_pSource,
+		IMFAttributes* m_pAttributes,
+		IMFActivate** m_ppDevices);
+	void CreateSourceReader(IMFMediaSource* m_pAUDSource);
 	~Media();
+private:
+	IMFMediaSource* m_pVIDSource;
+	IMFAttributes* m_pVIDAttributes;
+	IMFActivate** m_ppVIDDevices;
+	IMFMediaSource* m_pAUDSource;
+	IMFAttributes* m_pAUDAttributes;
+	IMFActivate** m_ppAUDDevices;
+	IMFSourceReader* m_pReader;
 };
 
