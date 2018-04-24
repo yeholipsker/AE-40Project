1a60b352685ed3e671f883da60759c5e5c66aac1
Matanel <User@USER-THINK>
2018-04-24 13:14:28 +0300

    Read Sample & Write Sink.

--- Media/Media.cpp
+++ Media/Media.cpp
@@ -7,6 +7,7 @@ Media::Media()
 	m_pVIDSource = NULL;
 	m_pAUDSource = NULL;
 	m_pReader = NULL;
+	m_pSinkWriter = NULL;
 }
 
 void Media::createMediaFile()
@@ -20,6 +21,8 @@ void Media::createMediaFile()
 
 	std::cout << "EnumerateDevices for both audio & video is done." << std::endl;
 
+/* ********************************************************************************************
+							//TODO - Do it for an aggregation media.//
 	// Create a collection of audio & video sources.
 	IMFCollection* pCollection = NULL;
 	MFCreateCollection(&pCollection);
@@ -34,34 +37,21 @@ void Media::createMediaFile()
 
 	// Create source reader for the media source. 
 	//CreateSourceReader(pAggSource); // TODO - Put inside an 'if SUCCEEDED' statement.
-	CreateSourceReader(m_pAUDSource);
+	// TODO - Do it also for the m_pVIDSource.
+	//CreateSourceReader(m_pVIDSource);
+	*********************************************************************************************
+*/
+	HRESULT hr = MFCreateSourceReaderFromMediaSource(m_pVIDSource, NULL, &m_pReader);
 
-	std::cout << "Create source reader done." << std::endl;
-}
+	std::cout << "Create source reader is done." << std::endl;
 
-HRESULT Media::EnumerateTypesForStream(IMFSourceReader *pReader, DWORD dwStreamIndex)
-{
-	HRESULT hr = S_OK;
-	DWORD dwMediaTypeIndex = 0;
+	// Create sink writer.
+	DWORD streamIndex = NULL;
+	CreateSinkWriter(&streamIndex);
 
-	while (SUCCEEDED(hr))
-	{
-		IMFMediaType *pType = NULL;
-		hr = m_pReader->GetNativeMediaType(dwStreamIndex, dwMediaTypeIndex, &pType);
-		if (hr == MF_E_NO_MORE_TYPES)
-		{
-			hr = S_OK;
-			break;
-		}
-		else if (SUCCEEDED(hr))
-		{
-			// Examine the media type. (Not shown.)
+	// Write media to a file.
+	WriteToFile(&streamIndex);
 
-			pType->Release();
-		}
-		++dwMediaTypeIndex;
-	}
-	return hr;
 }
 
 void Media::EnumerateDevices(GUID deviceType)
@@ -130,23 +120,113 @@ void Media::CreateSourceReader(IMFMediaSource * pSource)
 
 	if (SUCCEEDED(hr))
 	{
-		//CreateMediaFileForReader(m_pReader); TODO - IMPLEMENT.
-
-		IMFMediaType *pType = NULL;
-		DWORD dwStreamIndex = MF_SOURCE_READER_FIRST_AUDIO_STREAM;
-
-		m_pReader->GetCurrentMediaType(dwStreamIndex, &pType);
-
+		std::cout << "SUCCEED!" << std::endl;
+		
+		/*
 		HRESULT hr = EnumerateTypesForStream(m_pReader, dwStreamIndex);
+		*/
 
 		//m_pReader->Release();
 	}
-	else
-	{
-
-	}
 }
 
-Media::~Media()
+HRESULT Media::EnumerateTypesForStream(IMFSourceReader *pReader, DWORD dwStreamIndex)
 {
+	HRESULT hr = S_OK;
+	DWORD dwMediaTypeIndex = 0;
+
+	while (SUCCEEDED(hr))
+	{
+		IMFMediaType *pType = NULL;
+		hr = m_pReader->GetNativeMediaType(dwStreamIndex, dwMediaTypeIndex, &pType);
+		if (hr == MF_E_NO_MORE_TYPES)
+		{
+			hr = S_OK;
+			break;
+		}
+		else if (SUCCEEDED(hr))
+		{
+			// Examine the media type. (Not shown.)
+
+			pType->Release();
+		}
+		++dwMediaTypeIndex;
+	}
+	return hr;
 }
+
+void Media::CreateSinkWriter(DWORD *pStreamIndex)
+{
+	IMFMediaType* pMediaType = NULL;
+	DWORD dwStreamIndex = MF_SOURCE_READER_FIRST_VIDEO_STREAM;
+	m_pReader->GetCurrentMediaType(dwStreamIndex, &pMediaType);
+	HRESULT hr = MFCreateSinkWriterFromURL(L"output.wmv", NULL, NULL, &m_pSinkWriter);
+
+	if (SUCCEEDED(hr))
+	{
+		dwStreamIndex = NULL;
+		hr = m_pSinkWriter->AddStream(pMediaType, &dwStreamIndex);
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		hr = m_pSinkWriter->SetInputMediaType(dwStreamIndex, pMediaType, NULL);
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		hr = m_pSinkWriter->BeginWriting();
+	}
+
+	if (SUCCEEDED(hr))
+	{
+		*pStreamIndex = dwStreamIndex;
+	}
+
+}
+
+void Media::WriteToFile(DWORD* pStreamIndex)
+{
+	IMFSample* pSample = NULL;
+	DWORD stIndex = NULL;
+	DWORD flags = NULL;
+	LONGLONG llTstamp = NULL;
+	for (int i = 0; i < 10; i++)
+	{
+		HRESULT hr = m_pReader->ReadSample(MF_SOURCE_READER_ANY_STREAM, 0, &stIndex, &flags, &llTstamp, &pSample);
+
+		wprintf(L"Stream %d (%I64d)\n", stIndex, llTstamp);
+
+		if (flags & MF_SOURCE_READERF_ENDOFSTREAM)
+		{
+			wprintf(L"\tEnd of stream\n");
+		}
+		if (flags & MF_SOURCE_READERF_NEWSTREAM)
+		{
+			wprintf(L"\tNew stream\n");
+		}
+		if (flags & MF_SOURCE_READERF_NATIVEMEDIATYPECHANGED)
+		{
+			wprintf(L"\tNative type changed\n");
+		}
+		if (flags & MF_SOURCE_READERF_CURRENTMEDIATYPECHANGED)
+		{
+			wprintf(L"\tCurrent type changed\n");
+		}
+		if (flags & MF_SOURCE_READERF_STREAMTICK)
+		{
+			wprintf(L"\tStream tick\n");
+		}
+
+
+		if (SUCCEEDED(hr))
+		{
+			hr = m_pSinkWriter->WriteSample(*pStreamIndex, pSample);
+			std::cout << "Wrote sample!!" << std::endl;
+		}
+	}
+
+	m_pSinkWriter->Finalize();
+}
+
+Media::~Media() { }
--- Media/Media.h
+++ Media/Media.h
@@ -11,11 +11,14 @@ public:
 	void createMediaFile();//TODO change name
 	void EnumerateDevices(GUID deviceType);
 	void CreateSourceReader(IMFMediaSource* m_pAUDSource);
-	HRESULT EnumerateTypesForStream(IMFSourceReader *pReader, DWORD dwStreamIndex);
+	HRESULT EnumerateTypesForStream(IMFSourceReader* pReader, DWORD dwStreamIndex);
+	void CreateSinkWriter(DWORD* pStreamIndex);
+	void WriteToFile(DWORD* pStreamIndex);
 	~Media();
 private:
 	IMFMediaSource* m_pVIDSource;
 	IMFMediaSource* m_pAUDSource;
 	IMFSourceReader* m_pReader;
+	IMFSinkWriter* m_pSinkWriter;
 };
 
