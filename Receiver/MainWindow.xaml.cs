f5425526f4a94b8b05267faa4d2e309444a260d7
Matanel <User@USER-THINK>
2018-06-17 02:28:16 +0300

    "Add new" button - add up to 4 windows.

--- Receiver/MainWindow.xaml.cs
+++ Receiver/MainWindow.xaml.cs
@@ -8,7 +8,9 @@ namespace Vlc.DotNet.Wpf.Samples
     public partial class MainWindow
     {
         public const String RTP_ADDRESS = @"rtp:\\@224.1.1.1:5004";
-        public const int maxNumberOfWindows = 4;
+        public const String FILE_ADDRESS = @"C:\Users\User\Downloads\100_3392.JPG";
+        public const String PATH = @"\..\..\VLC\";
+        public const int MAX_NUMBER_OF_WINDOWS = 4;
 
         private int numberOfWindows = 0;
 
@@ -16,15 +18,16 @@ namespace Vlc.DotNet.Wpf.Samples
         {
             InitializeComponent();
             vlcPlayer.MediaPlayer.VlcLibDirectory =
-                new DirectoryInfo(Directory.GetCurrentDirectory() + @"\..\..\VLC\");
+                new DirectoryInfo(Directory.GetCurrentDirectory() + PATH);
 
             vlcPlayer.MediaPlayer.EndInit();
-            vlcPlayer.MediaPlayer.Play(new Uri(RTP_ADDRESS));
+            vlcPlayer.MediaPlayer.Play(new Uri(@"C:\Users\User\Desktop\1.png"));
             numberOfWindows++;
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
+            
             if (StopButton.Content.ToString() == "Stop")
             {
                 vlcPlayer.MediaPlayer.Stop();
@@ -35,58 +38,62 @@ namespace Vlc.DotNet.Wpf.Samples
                 vlcPlayer.MediaPlayer.Play(new Uri(RTP_ADDRESS));
                 StopButton.Content = "Stop";
             }
+        }
 
-            // ==================== THE ADD PLAYER PART ==================== //
-            if (++numberOfWindows >= maxNumberOfWindows)
+        private void AddNewButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (++numberOfWindows <= MAX_NUMBER_OF_WINDOWS)
             {
+                // Add new VlcPlayer.
+                VlcControl player = new VlcControl();
+                player.MediaPlayer.VlcLibDirectory =
+                    new DirectoryInfo(Directory.GetCurrentDirectory() + PATH);
 
+                player.BeginInit();
+                player.EndInit();
+
+                // Change the screen.
+                Console.WriteLine("numberOfWindows = " + numberOfWindows);
+                switch (numberOfWindows)
+                {
+                    case 2:
+                        ColumnDefinition newColTop = new ColumnDefinition();
+                        newColTop.Width = new GridLength(400);
+                        ScreenGridTop.ColumnDefinitions.Add(newColTop);
+
+                        vlcPlayer2.MediaPlayer.VlcLibDirectory =
+                            new DirectoryInfo(Directory.GetCurrentDirectory() + PATH);
+                        vlcPlayer2.MediaPlayer.EndInit();
+                        vlcPlayer2.MediaPlayer.Play(new Uri(@"C:\Users\User\Desktop\2.png"));
+
+                        break;
+
+                    case 3:
+                        RowDefinition newRow = new RowDefinition();
+                        newRow.Height = new GridLength(250);
+                        ScreenGrid.RowDefinitions.Add(newRow);
+
+                        vlcPlayer3.MediaPlayer.VlcLibDirectory =
+                            new DirectoryInfo(Directory.GetCurrentDirectory() + PATH);
+
+                        vlcPlayer3.MediaPlayer.EndInit();
+                        vlcPlayer3.MediaPlayer.Play(new Uri(@"C:\Users\User\Desktop\3.png"));
+                        break;
+                    case 4:
+                        ColumnDefinition newColDown = new ColumnDefinition();
+                        newColDown.Width = new GridLength(400);
+                        ScreenGridDown.ColumnDefinitions.Add(newColDown);
+
+                        vlcPlayer4.MediaPlayer.VlcLibDirectory =
+                            new DirectoryInfo(Directory.GetCurrentDirectory() + PATH);
+
+                        vlcPlayer4.MediaPlayer.EndInit();
+                        vlcPlayer4.MediaPlayer.Play(new Uri(@"C:\Users\User\Desktop\4.png"));
+                        break;
+                    default:
+                        break;
+                }
             }
-
-            // Add new VlcPlayer.
-            VlcControl player = new VlcControl();
-            player.MediaPlayer.VlcLibDirectory =
-                new DirectoryInfo(Directory.GetCurrentDirectory() + @"\..\..\VLC\");
-
-            player.BeginInit();
-            player.EndInit();
-            vlcPlayer.MediaPlayer.Play(new Uri(RTP_ADDRESS));
-
-            // Change the screen.
-            switch (numberOfWindows)
-            {
-                case 2:
-                    Console.WriteLine("Case 2");
-                    ColumnDefinition newCol = new ColumnDefinition();
-                    newCol.Width = new GridLength(400);
-                    ScreenGrid.ColumnDefinitions.Add(newCol);
-                    ScreenGrid.Children.Add(player);
-                    Grid.SetColumn(player, ScreenGrid.RowDefinitions.Count);
-                    break;
-                case 3:
-                    Console.WriteLine("Case 3");
-                    RowDefinition newRow = new RowDefinition();
-                    newRow.Height = new GridLength(250);
-                    ScreenGrid.RowDefinitions.Add(newRow);
-                    ScreenGrid.Children.Add(player);
-                    Grid.SetRow(player, ScreenGrid.RowDefinitions.Count);
-                    Grid.SetColumn(player, ScreenGrid.RowDefinitions.Count);
-                    break;
-                case 4:
-                    Console.WriteLine("Case 4");
-                    ScreenGrid.Children.Add(player);
-                    Grid.SetRow(player, ScreenGrid.RowDefinitions.Count);
-                    Grid.SetColumn(player, ScreenGrid.RowDefinitions.Count - 2);
-                    break;
-                default:
-                    break;
-            }
-
-            
-            player.MediaPlayer.Play(new Uri(RTP_ADDRESS));
-
-            // ============================================================== //
-            
-
         }
     }
 }
